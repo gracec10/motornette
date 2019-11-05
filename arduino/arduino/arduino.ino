64c9d1814a2e8088703397d4afc9e0efea61573f
Sabrina Long <sabrina.long3@gmail.com>
2019-11-04 23:11:15 -0500

    Adding comments to processing/arduino code

--- arduino/arduino/arduino.ino
+++ arduino/arduino/arduino.ino
@@ -3,68 +3,74 @@
 //data received from serial port
 char val;
 
+//pins
+const int LLpin = 13;
+const int RLpin = 12;
+const int LApin = 15;
+const int RApin = 14;
+
 //servos
 Servo LLServo;
 Servo RLServo;
 Servo LAServo;
-Servo RAServo;  
+Servo RAServo;
 
 void setup() {
   Serial.begin(115200);
 
   //pins
-  LLServo.attach(13);
-  RLServo.attach(12);
-  LAServo.attach(15);
-  RAServo.attach(14);
+  LLServo.attach(LLpin);
+  RLServo.attach(RLpin);
+  LAServo.attach(LApin);
+  RAServo.attach(RApin);
 
   //initial positioning
-  LLServo.write(0);  
+  LLServo.write(0);
   RLServo.write(0);
   LAServo.write(0);
   RAServo.write(0);
 }
 
-void loop(){
+void loop() {
   if (Serial.available()) {
     val = Serial.read();
   }
-  
-//left arm
-  if (val == '1'){
+
+  //left arm
+  if (val == '1') {
     LAServo.write(180);
     delay(500);
     LAServo.write(0);
     delay(300);
   }
 
-//right arm
-  if (val == '2'){
+  //right arm
+  if (val == '2') {
     RAServo.write(180);
     delay(500);
     RAServo.write(0);
     delay(300);
   }
 
-//right leg
-  if (val == '3'){
+  //right leg
+  if (val == '3') {
     RLServo.write(180);
     delay(500);
     RLServo.write(0);
     delay(300);
   }
 
-//left leg
-  if (val == '4'){
-    LLServo.write(180);  
+  //left leg
+  if (val == '4') {
+    LLServo.write(180);
     delay(500);
     LLServo.write(0);
     delay(300);
   }
 
-//stop
-  if (val == '0'){
-    LLServo.write(0);  
+  //stop
+  if (val == '0') {
+    LLServo.write(0);
     RLServo.write(0);
     LAServo.write(0);
     RAServo.write(0);
--- motornett/motornett.pde
+++ motornett/motornett.pde
@@ -5,12 +5,15 @@ String input;
 char[] dance = {};
 
 void setup() {
+  // Use the Serial.list() printout to find the Serial location of your ESP32
+  // Replace [11] in line 12 with the index that corresponds with your ESP32
+
   printArray(Serial.list());
   myPort = new Serial(this, Serial.list()[11], 115200);
 }
 
 void draw() {  
-    if (myPort.available() > 0){
+  if (myPort.available() > 0) {
     input = myPort.readString();
     println(input);
   }
@@ -18,31 +21,31 @@ void draw() {
 
 void keyPressed() {
   //left arm
-  if (keyCode == LEFT){
+  if (keyCode == LEFT) {
     dance = append(dance, '1');
   }
-  
+
   //right arm
-  if (keyCode == RIGHT){
+  if (keyCode == RIGHT) {
     dance = append(dance, '2');
   }
-  
+
   //left leg
-  if (keyCode == UP){
+  if (keyCode == UP) {
     dance = append(dance, '3');
   }
-  
+
   //right leg
-  if (keyCode == DOWN){
+  if (keyCode == DOWN) {
     dance = append(dance, '4');
   }
-  
+
   //end your dance
-  if (keyCode == SHIFT){
+  if (keyCode == SHIFT) {
     dance = append(dance, '0');
-    for (int i = 0; i < dance.length; i++){
+    for (int i = 0; i < dance.length; i++) {
       print(dance[i]);
       myPort.write(dance[i]);
+    }
   }
-}
 }
